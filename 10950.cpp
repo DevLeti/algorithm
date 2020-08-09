91b1b10695f7baf05fd31b301cd8ac4a0cab8e69
DevLETi <devleti0106@gmail.com>
2020-08-09 20:07:51 +0900

    2020-08-09

---
+++ 10950.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	/*10951번 코드랑 동일. test case 개수 input 버림.*/
+	int waste;
+	cin >> waste;
+	cin.ignore();
+
+	vector<string> input_v;
+	string input;
+	while (getline(cin, input))
+	{
+		input_v.push_back(input);
+	}
+
+	int sum;
+	int a, b;
+	for (int i = 0; i < input_v.size(); i++)
+	{
+		a = (int)input_v[i].at(0) - '0';
+		b = (int)input_v[i].at(2) - '0';
+		sum = a + b;
+		cout << sum << endl;
+	}
+
+	return 0;
+}
